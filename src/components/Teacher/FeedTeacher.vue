9c37ca0aac9d9acf693c11ccc5eb5602dfd49066
Sviatoslav Yaroshenko <quex.modest@gmail.com>
2024-01-16 01:22:56 +0200

    fixed input

--- src/components/Teacher/FeedTeacher.vue
+++ src/components/Teacher/FeedTeacher.vue
@@ -9,28 +9,39 @@ import {IonPage, IonContent, IonFooter} from "@ionic/vue";
   </ion-content>
 
   <ion-footer>
-    <div class="container-footer">
-      <v-text-field variant="outlined" label="Коментар" class="text-from-teacher"></v-text-field>
-      <v-btn elevation="0" icon="mdi-send" class="btn-send-comment"></v-btn>
-    </div>
+
+
+        <div class="container-footer">
+          <v-text-field
+              label="Коментар"
+              class="text-from-teacher"
+              variant="plain"
+          >
+          </v-text-field>
+          <v-btn elevation="0" icon="mdi-send" class="btn-send-comment"></v-btn>
+        </div>
+
+
   </ion-footer>
 </ion-page>
 </template>
 
 <style scoped>
-.container-footer{
+.container-footer {
   width: 90%;
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 10px auto;
 }
-.text-from-teacher{
+
+.text-from-teacher {
   width: 90%;
   margin: 20px auto 0 auto;
   box-sizing: border-box;
 }
-.btn-send-comment{
+
+.btn-send-comment {
   margin: 0 10px;
 }
 </style>
